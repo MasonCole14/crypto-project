9f6772140f787bff31297eaea5924aaecc7c1a1f
Mason Cole <cole.mason2@gmail.com>
2023-07-13 22:21:17 -0500

    updates

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinPage = () => {
-  return (
-    <div>CoinPage</div>
-  )
-}
+  const [coin, setCoin] = useState({});
 
-export default CoinPage
+  const url =
+    "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setCoin(res.data);
+      console.log(res.data);
+    });
+  }, [url]);
+
+  return (
+    <div>
+      <div>
+        <img src={coin.image?.large} alt="/" />
+        <div>
+          <p>{coin?.name} price</p>
+          <p>{coin?.symbol?.toUpperCase()} / USD</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            {coin.market_data?.current_price ? (
+              <p>${coin.market_data.current_price.usd.toLocaleString()}</p>
+            ) : null}
+            <p> 7 Day </p>
+          </div>
+          <div>
+            <Sparklines data={coin.market_data?.sparkline_7d.price}>
+              <SparklinesLine color="teal" />
+            </Sparklines>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoinPage;
